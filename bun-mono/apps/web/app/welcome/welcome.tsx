a2aca52877758df620884937a8fc8be6995e1a3c
smit <smitmistry19@gmail.com>
2025-03-20 11:00:11 +0530

    updated web page for experiment

--- bun-mono/apps/web/app/welcome/welcome.tsx
+++ bun-mono/apps/web/app/welcome/welcome.tsx
@@ -1,7 +1,37 @@
+import { Button } from "@repo/ui/components/button";
+import { Card, CardContent } from "@repo/ui/components/card";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@repo/ui/components/tabs";
+
 export function Welcome() {
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
-      <h1 className="text-5xl font-semibold tracking-tight">Hello there!</h1>
+      <div className="flex flex-col gap-3 w-full max-w-md">
+        <Tabs defaultValue="one">
+          <TabsList>
+            <TabsTrigger value="one">One</TabsTrigger>
+            <TabsTrigger value="two">Two</TabsTrigger>
+          </TabsList>
+          <TabsContent value="one">
+            <Card>
+              <CardContent>
+                <Button>Hello there one!</Button>
+              </CardContent>
+            </Card>
+          </TabsContent>
+          <TabsContent value="two">
+            <Card>
+              <CardContent>
+                <Button variant={"outline"}>Hello there two!</Button>
+              </CardContent>
+            </Card>
+          </TabsContent>
+        </Tabs>
+      </div>
     </main>
   );
 }
