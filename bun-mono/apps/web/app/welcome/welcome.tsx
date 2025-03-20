00317201555c6c65f5876dac799ba5da4383b4b9
smit <smitmistry19@gmail.com>
2025-03-20 10:47:08 +0530

    added apps/web with react-router

---
+++ bun-mono/apps/web/app/welcome/welcome.tsx
@@ -0,0 +1,7 @@
+export function Welcome() {
+  return (
+    <main className="flex items-center justify-center pt-16 pb-4">
+      <h1 className="text-5xl font-semibold tracking-tight">Hello there!</h1>
+    </main>
+  );
+}
